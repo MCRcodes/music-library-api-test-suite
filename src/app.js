e2911f46f85df2c8cc98ce34f5feaf8d509feb06
Holly Fanthorpe <hollyfanthorpe@gmail.com>
2019-09-18 20:57:55 +0100

    Created funtion to add artists to database with post request

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const artistsController = require('../src/controllers/artists');
 
 const app = express();
+app.use(express.json());
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
 });
 
-
-app.post('/artists', (req, res) => {
-  res.status(201).send();
-});
+app.post('/artists', artistsController.create);
 
 module.exports = app;
