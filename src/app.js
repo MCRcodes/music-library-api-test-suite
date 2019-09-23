b5acb525c26b813f5da590017c74203c9b82bf56
Cai Jenkins <Caisjenkins@gmail.com>
2019-09-23 19:53:29 +0100

    wrote and passed album tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const artistControllers = require('./controllers/artists.js');
+const albumControllers = require('./controllers/albums.js');
 
 const app = express();
 app.use(express.json());
@@ -8,5 +9,10 @@ app.post('/artists', artistControllers.create);
 app.get('/artists', artistControllers.list);
 app.get('/artists/:artistId', artistControllers.find);
 app.patch('/artists/:artistId', artistControllers.patch);
+app.delete('/artists/:artistId', artistControllers.delete);
+app.post('/artists/:artistId/albums', albumControllers.create);
+app.get('/artists/:artistId/albums', albumControllers.list);
+app.patch('/artists/:artistId/albums/:albumId', albumControllers.patch);
+app.delete('/artists/:artistId/albums/:albumId', albumControllers.delete);
 
 module.exports = app;
